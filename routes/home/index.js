2812a24c0873ca64c6c5547748c2f2a84ab8e040
alaketych <alaketych@ukr.net>
2019-12-29 19:48:27 +0200

    registration validation

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/User')
 const Post = require('../../models/Post')
 const Category = require('../../models/Category')
 
@@ -26,6 +27,64 @@ router.get('/login', (request, response) => {
 
 router.get('/register', (request, response) => {
     response.render('./home/register')
+})
+
+router.post('/register', (request, response) => {
+    let errors = []
+
+    const newUser = new User ({
+        firstName   : request.body.firstName,
+        lastName    : request.body.lastName,
+        email       : request.body.email,
+        password    : request.body.password,
+    })
+
+    if(!request.body.firstName) {
+        errors.push({
+            message: 'Please enter your first name.'
+        })
+    }
+
+    if(!request.body.lastName) {
+        errors.push({
+            message: 'Please enter your last name.'
+        })
+    }
+
+    if(!request.body.email) {
+        errors.push({
+            message: 'Please enter your email'
+        })
+    }
+
+    if(!request.body.password && !request.body.passwordConfirm) {
+        errors.push({
+            message: 'One of the passwords filds was not be entered.'
+        })
+    } 
+
+    if(request.body.password !== request.body.passwordConfirm) {
+        errors.push({
+            message: 'Original passwords does not match with the confirmed password. Try again.'
+        })
+    }
+    
+    if(errors.length > 0) {
+        response.render('home/register', {
+            errors: errors
+        })
+    }
+    else {
+        const newUser = new User ({
+            firstName   : request.body.firstName,
+            lastName    : request.body.lastName,
+            email       : request.body.email,
+            password    : request.body.password,
+        })
+
+        newUser.save().then(savedUser => {
+            response.send('User was successfully added.')      
+        })}
 })
 
 router.get('/post/:id', (request, response) => {
